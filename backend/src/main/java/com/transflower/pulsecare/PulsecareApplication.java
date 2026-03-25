732abfcfc7c32f76ba6a5b0a1edb2bf165653ead
salonipawale <pawalesaloni123@gmail.com>
2026-03-25 23:32:50 +0530

    initialize Spring Boot backend with layered architecture (controller, service, repository, entity) and dependencies (web, jpa, mysql, security)

---
+++ backend/src/main/java/com/transflower/pulsecare/PulsecareApplication.java
@@ -0,0 +1,13 @@
+package com.transflower.pulsecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsecareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/transflower/pulsecare/PulsecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transflower.pulsecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
